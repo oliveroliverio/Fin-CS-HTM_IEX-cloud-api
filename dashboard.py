a5d5d54c95db5524593cbb7c547fc51bcd3f41ed
Oliver Oliverio <oliveroliverio@gmail.com>
2021-11-18 21:55:31 -0800

    20211118 able to get quotes

--- dashboard.py
+++ dashboard.py
@@ -1,17 +1,23 @@
 import streamlit as st
 import requests
+import config
 
 st.title("Fundamentals Dashboard")
 
 symbol = st.sidebar.text_input("Symbol", value="AAPL")
 
-screen = st.sidebar.selectbox("View", ('Overiew', 'Fundamentals', 'News', 'Ownership', 'Technicals'))
-# st.write(symbol)
+screen = st.sidebar.selectbox("View", ('Overview', 'Fundamentals', 'News', 'Ownership', 'Technicals'))
+st.write(symbol)
 st.title(screen)
 
 if screen == 'Overview':
-  # get stock info via api key
-  url = "https://cloud.iexapis.com/stable/stock/aapl/quote?token={config.IEX_API_TOKEN}"
+    # get stock info via api key
+    url = f"https://cloud.iexapis.com/stable/stock/aapl/quote?token={config.IEX_API_TOKEN}"
+    r = requests.get(url)
+    print(r.json())
+    # response_json = r.json()
+    # st.image(response_json['url'])
 
-if screen == 'Fundamentals':
-  pass
+    # if screen == 'Fundamentals':
+    #   pass
+
